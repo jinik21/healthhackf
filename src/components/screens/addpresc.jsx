48aea401a842184ffc4fbdde59368d0abb38f32d
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2022-02-04 13:55:17 +0530

    list made add prescription

--- src/components/screens/addpresc.jsx
+++ src/components/screens/addpresc.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
 import firebase from "../../firebase";
+import { Page, Text, View, Document, StyleSheet, ReactPDF  } from '@react-pdf/renderer';
 
 var storageRef = firebase.storage().ref();
 const toBlob = (file) => {
@@ -21,18 +23,38 @@ class AddPresc extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: "",
-      selectedFile: null,
-      uemail:"",
-      sid:""
+        data: "",
+        selectedFile: null,
+        uemail:"",
+        sid: "",
+        medname: "",
+        dosage: "",
+        days: "",
+        times: "",
+        presc: [],
     };
-  }
+    }
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onAdd = (e) => {
+        e.preventDefault();
+        console.log(this.state.presc);
+        const obj = new Object({
+            medname: this.state.medname,
+            dosage: this.state.dosage,
+            days: this.state.days,
+            times: this.state.times,
+        });
+        this.setState({presc:[...this.state.presc,obj]});
+        console.log(this.state.presc);
+    }
   onFile = (e) => {
     console.log(e.target.files[0]);
     this.setState({ selectedFile: e.target.files[0] });
-  };
-  onFileUpload = async () => {
-    const formData = new FormData();
+    };
+    onFileUpload = async () => {
+    /*const formData = new FormData();
     if (this.state.selectedFile == null) {
       alert("No file Selected!!!");
     } else {
@@ -71,7 +93,7 @@ class AddPresc extends React.Component {
       } catch (err) {
         console.log(err);
       }
-    }
+    }*/
   };
   componentDidMount=()=>{
     console.log(this.props.match.params.email);
@@ -136,61 +158,103 @@ class AddPresc extends React.Component {
               className="col main pt-5 mt=3 border border-dark"
               style={{ backgroundColor: "#FAF3F3" }}
             >
-              <h1 className="text-dark">Upload Patient Prescriptions</h1>
-              <p className="lead d-none d-sm-block">Upload Prescription for Email: {this.state.uemail} and SessionId: {this.state.sid}</p>
-              <div className="row">
-                <div className="col-md-6 mb-4">
-                  <div className="form-outline">
-                    <input
-                      type="file"
-                      className="form-control"
-                      id="customFile"
-                      name="licensedoc"
-                      onChange={this.onFile}
-                    />
-                    <label className="form-label text-light" htmlFor="customFile">
-                      Upload Routine(pdf)
-                    </label>
-                  </div>
-                </div>
-                <div className="col-md-6 mb-4">
-                  <div className="form-outline">
-                    <button
-                            type="button"
-                            className="btn btn-warning btn-lg ms-2 b1-color"
-                            onClick={this.onFileUpload}
-                          >
-                            Upload
-                          </button>
-                  </div>
-                </div>
-               </div>
+              <h1 className="text-dark">Add Patient Prescriptions</h1>
+                        <p className="lead d-none d-sm-block">Add Prescription for Email: {this.state.uemail} and SessionId: {this.state.sid}</p>
+                        <div className="row">
+                            <div className="col-md-6 mb-4" style={{ width: "25%" }}>
+                                <div class="mb-3">
+                                    <input type="text" name="medname" class="form-control" onChange={this.onChange} value={this.medname} placeholder="Medicine Name" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                <div class="mb-3">
+                                    <input type="text" name="dosage" class="form-control" onChange={this.onChange} value={this.dosage} placeholder="Dosage" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                <div class="mb-3">
+                                    <input type="text" name="days" class="form-control" onChange={this.onChange} value={this.days} placeholder="Days" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                <div class="mb-3">
+                                    <input stype="text" name="times" class="form-control" onChange={this.onChange} value={this.times} placeholder="Times per Day" />
+                                </div>
+                            </div>
+                            <div className="col-md-6 mb-4" style={{ width: "20%" ,position:"relative",left:"40%"}}>
+                                <div className="form-outline">
+                                    <button
+                                        type="button"
+                                        className="btn btn-warning btn-lg ms-2 b2-color"
+                                        onClick={this.onAdd}
+                                    >
+                                        Add
+                                    </button>
+                                </div>
+                                <div className="col-md-6 mb-4" style={{ width: "20%", position: "relative", left: "40%",top:"-40%" }}>
+                                    <div className="form-outline">
+                                        <button
+                                            type="button"
+                                            className="btn btn-warning btn-lg ms-2 b2-color"
+                                            onClick={this.onFileUpload}
+                                        >
+                                            Upload
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div className="row">
                             <h1>List View</h1>
                         </div>
                         <div className="row">
-                            <div className="col-md-6 mb-4" style={{ width: "30%", justifyContent: "center" }}>
+                            <div className="col-md-6 mb-4" style={{ width: "30%" }}>
                                 <div class="mb-3">
-                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Medicine Name" disabled/>
+                                    <input style={{ backgroundColor: "#072227", textAlign: "center", color: "white", fontWeight: "bold", fontSize:"20px" }} type="text" id="disabledTextInput" class="form-control" value="Medicine Name" disabled />
                                  </div>
                             </div>
                             <div className="col-md-6 mb-4" style={{ width: "20%" }}>
                                 <div class="mb-3">
-                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Dosage" disabled />
+                                    <input style={{ backgroundColor: "#072227", textAlign: "center", color: "white", fontWeight: "bold", fontSize: "20px"}} type="text" id="disabledTextInput" class="form-control" value="Dosage" disabled />
                                 </div>
                             </div>
                             <div className="col-md-6 mb-4" style={{ width: "20%" }}>
                                 <div class="mb-3">
-                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Days" disabled />
+                                    <input style={{ backgroundColor: "#072227", textAlign: "center", color: "white", fontWeight: "bold", fontSize: "20px" }} type="text" id="disabledTextInput" class="form-control" value="Days" disabled />
                                 </div>
                             </div>
                             <div className="col-md-6 mb-4" style={{ width: "20%" }}>
                                 <div class="mb-3">
-                                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Times per Day" disabled />
+                                    <input style={{ backgroundColor: "#072227", textAlign: "center", color: "white", fontWeight: "bold", fontSize: "20px" }} stype="text" id="disabledTextInput" class="form-control" value="Times per Day" disabled />
                                 </div>
                             </div>
                         </div>
-
+                        {this.state.presc.length === 0 ? <h1></h1> : this.state.presc.map((ele, i) => (
+                            <div key={i} className="row">
+                                <div className="col-md-6 mb-4" style={{ width: "30%",margin:"0px" }}>
+                                    <div class="mb-3">
+                                        <input style={{ backgroundColor: "#35858B", textAlign: "center", color: "white", fontSize: "15px" }} type="text" id="disabledTextInput" class="form-control" value={ele.medname} disabled />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                    <div class="mb-3">
+                                        <input style={{ backgroundColor: "#35858B", textAlign: "center", color: "white", fontSize: "15px" }} type="text" id="disabledTextInput" class="form-control" value={ele.dosage} disabled />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                    <div class="mb-3">
+                                        <input style={{ backgroundColor: "#35858B", textAlign: "center", color: "white", fontSize: "15px" }} type="text" id="disabledTextInput" class="form-control" value={ele.days} disabled />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-4" style={{ width: "20%" }}>
+                                    <div class="mb-3">
+                                        <input style={{ backgroundColor: "#35858B", textAlign: "center", color: "white", fontSize: "15px" }} stype="text" id="disabledTextInput" class="form-control" value={ele.times} disabled />
+                                    </div>
+                                </div>
+                            </div>
+                            ))
+                            
+                            }
             </div>
           </div>
         </div>
